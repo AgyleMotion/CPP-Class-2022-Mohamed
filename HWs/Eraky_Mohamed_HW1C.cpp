b9c4e76a4199b0d67ff81c64eed69e1fb763ba62
AgyleMotion <meraky@stevens.edu>
2022-09-05 00:02:49 -0400

    Summing Numbers from 1 to 100

---
+++ HWs/Eraky_Mohamed_HW1C.cpp
@@ -0,0 +1,44 @@
+
+/*
+***********************
+Author : Mohamed Eraky
+***********************
+This program sums the numbers from 1 to 100 using Gauss formula and for-loop.
+*/
+
+#include <iostream>
+
+int main()
+{
+   
+    int n = 100;
+    int G_sum = 0; 
+    int L_sum = 0;
+
+    G_sum = n*(n+1)/2 ;
+    
+    std::cout<< "Summing numbers from 1 to 100 : " <<std::endl; 
+    
+    // Calculate the answer using loop
+
+    for (int i= 1; i<=100;i++){
+        
+         L_sum=L_sum+i;
+
+    }
+   
+
+    if ( L_sum == G_sum){
+        
+        std::cout<< " Gauss sum : " <<  G_sum << std::endl;
+        std::cout<< " Loop sum : " << L_sum <<std:: endl;
+        std::cout<< " Both results are the same! " << std::endl; 
+
+    }else{
+       std::cout<< " Results doesn't match! " << std::endl; 
+    }
+  
+
+  return 0;
+}
+
